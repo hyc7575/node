d6e24affbd1c23f7b4874a4a9ee48e20a1d9bf51
T <T@192.168.204.57>
2015-11-19 17:47:44 +0900

    4일차

--- day4/movieDB_Exam/app.js
+++ day4/movieDB_Exam/app.js
@@ -71,7 +71,37 @@ app.get('/movies',function(req,res) {
 			res.render('movies.jade',{
 				title: 'Movies',
 				items: results
-			})
+			});
+		}
+	});
+});
+app.get('/movies/:id/update',function(req,res) {
+	var select = "select * from movie where movie_id=?";
+	connection.query(select,[req.params.id],function(err,results) {
+		if(err) {
+			console.log(err);
+		} else {
+			var movieObj = {};
+			if(results.length > 0) {
+				movieObj = {
+					movie_id: results[0].movie_id,
+					title: results[0].title,
+					director:results[0].director,
+					year:results[0].year,
+					comments: [],
+					synopsis:''
+				};
+			}
+			var movie = mongodb.collection('movie');
+			movie.find({movie_id:movieObj.movie_id}).toArray(function(err,docs) {
+				if(docs.length>0) {
+					movieObj.synopsis = docs[0].synopsis;
+				}
+				res.render('movie_update',{
+					title: '글 수정하기',
+					movie: movieObj
+				});
+			});
 		}
 	});
 });
@@ -80,9 +110,9 @@ app.get('/movies/:id',function(req,res) {
 	if(id === 'add') {
 		res.render('movies_add',{
 			title: '영화 추가하기'
-		})
+		});
 	} else {
-		var select = "select * from movie where movie_id=?;"
+		var select = "select * from movie where movie_id=?;";
 		connection.query(select,[id],function(err,results) {
 			if(err) {
 				console.log(err);
@@ -104,15 +134,53 @@ app.get('/movies/:id',function(req,res) {
 					//movie라는 콜렉션에서 find함수 실행.
 					//movie_id가 불러온 결과 값의 movie_id 와 같은 것만 배열로 변환해서 docs로 넘긴다.
 					movie.find({movie_id:Number(results[0].movie_id)}).toArray(function(err,docs) {
+						//몽고db는 배열형태로 바꿔주어야함.(필수는 아니고 그냥 이렇게 쓰자)
 						if(docs.length > 0) {
 							movieObj.synopsis = docs[0].synopsis;
 						}
-						res.render('movie.jade',{ movie: movieObj });
+						//코멘트라는 콜렉션을 지정해줍니당.
+						var comments = mongodb.collection('comments');
+						comments.find({movie_id:Number(results[0].movie_id)}).toArray(function(err,docs) {
+							for(var i = 0; i < docs.length; i++) {
+								console.log(movieObj);
+								movieObj.comments.push({comment: docs[i].comment, comment_id: docs[i]._id});
+							}
+							//여기서 랜더링해야지 코멘트를 출력할 수 있음. 여기 바로 밖에서 하면 출력후 코멘트를 저장하고 불러와서 코멘트가 출력하지 않음. 다른 로직 들도 마찬가지.
+							res.render('movie.jade',{ movie: movieObj });
+						});
 					});
 				}
 			}
 		});
 	}
+});
+app.post('/movies/update',function(req,res) {
+	var title = req.body.title;
+	var director = req.body.director;
+	var year = req.body.year;
+	var synopsis = req.body.synopsis;
+	var movie_id = Number(req.body.movie_id);
+	var update = "update movie set title=?, director=?, year=? where movie_id=?";
+	connection.query(update,[title,director,year,movie_id], function(err,results) {
+		if(err) {
+			console.log(err);
+		} else {
+			var movie = mongodb.collection('movie');
+			movie.update({movie_id:movie_id},{'$set':{synopsis:synopsis}},function(err,docs) {
+				res.writeHead(301,{'location':'/movies'});res.end();
+			});
+		}
+	});
+});
+app.post('/movies/comment',function(req,res) {
+	var comments = mongodb.collection('comments');
+	comments.insert({movie_id:Number(req.body.movie_id),comment: req.body.comment}, function(err, results) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.writeHead(301,{'location':'/movies/'+req.body.movie_id});res.end();
+		}
+	});
 });
 app.post('/movies/add',function(req,res) {
 	var title = req.body.title;
@@ -135,11 +203,39 @@ app.post('/movies/add',function(req,res) {
 		}
 	});
 });
-
-app.post('/movies/comment',function() {
-	
+var ObjectID = require('mongodb').ObjectID;
+app.del('/movies/comment_delete',function(req,res) {
+	var movie_id = Number(req.body.movie_id);
+	var comments = mongodb.collection('comments');
+	/*comments.remove({movie_id: movie_id},function(err,docs) {
+		res.writeHead(301,{'location':'/movies/'+movie_id});res.end();
+	});*/
+	//밑 선생님이 해주신것 중간에 못봐서;; 
+	console.log(req.body._id);
+	comments.remove({_id:new ObjectID(req.body._id)},function(err,results) {
+		if(err) console.log(err);
+		else {
+			res.redirect('/movies/'+req.body.movie_id)
+		}
+	});
 });
-
+app.del('/movies/delete',function(req,res) {
+	var movie_id = Number(req.body.movie_id);
+	var del = 'delete from movie where movie_id=?';
+	connection.query(del,[movie_id],function(err,results) {
+		if(err) {
+			console.log(err);
+		} else {
+			var movie = mongodb.collection('movie');
+			var comments = mongodb.collection('comments');
+			movie.remove({movie_id: movie_id},function(err,docs) {
+				comments.remove({movie_id: movie_id},function(err,docs) {
+					res.writeHead(301,{'location':'/movies'});res.end();
+				});
+			});
+		}
+	});
+});
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
